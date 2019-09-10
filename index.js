12325da69df36a4a06f70dae0dbc2a2fa76203c4
fernando <rluis4490@gmail.com>
2019-09-10 17:20:11 -0600

    integrated core ui

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+    res.sendFile(__dirname+'/public/coreui/index.html');
+  });
+  
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
